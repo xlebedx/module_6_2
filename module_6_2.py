f8afe3d4c2593400f93a09ef75d7b1e373ed3bf2
xlebedx <xlebedx@gmail.com>
2024-10-07 21:02:04 +0300

    Initial commit

---
+++ module_6_2.py
@@ -0,0 +1,53 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, model, color, engine_power):
+        self.owner = owner
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
+
+    def get_model(self):
+        return f"Модель: {self.__model}"
+
+    def get_horsepower(self):
+        return f"Мощность двигателя: {self.__engine_power}"
+
+    def get_color(self):
+        return f"Цвет: {self.__color}"
+
+    def print_info(self):  # можно ли записать этот вывод с использованием \n?
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f"Владелец: {self.owner}")
+
+    def set_color(self, new_color):
+        if new_color.lower() in [color.lower() for color in self.__COLOR_VARIANTS]:
+            self.__color = new_color
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+    def __init__(self, owner, model, color, engine_power):
+        super().__init__(owner, model, color, engine_power)
+
+    def get_passengers_limit(self):
+        return f"Ограничение по пассажирам: {self.__PASSENGERS_LIMIT}"
+
+
+if __name__ == '__main__':
+    # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+    vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+    # Изначальные свойства
+    vehicle1.print_info()
+
+    # Меняем свойства (в т.ч. вызывая методы)
+    vehicle1.set_color('Pink')
+    vehicle1.set_color('BLACK')
+    vehicle1.owner = 'Vasyok'
+
+    # Проверяем что поменялось
+    vehicle1.print_info()
